522bfccdb1b7f719bd06a29c1f5c04717c430a6e
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-06 08:06:22 +0300

    Completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+      # initialization
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        
+        # attribute to keep track of the last transaction
+        self.lastTransaction = 0
+
+    def add_item(self, title, price, quantity=None):
+      # if quantity is provided
+        if quantity:
+            self.total += price * quantity
+            
+            # append each item to items list
+            for item in range(quantity):
+                self.items.append(title)
+
+            # update lastTransaction
+            self.lastTransaction = price * quantity
+        else:
+            self.total += price
+
+            self.items.append(title)
+
+            self.lastTransaction = price
+
+    def apply_discount(self):
+      # if discount is provided
+        if self.discount:
+            self.total -= int(self.discount/100 * self.total)
+            
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+      # subtracts the last item from the total
+        self.total -= self.lastTransaction
